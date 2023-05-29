a99899666fe88355d5b3d2013776fa79c5e40a7f
Teona <tekizashvili@gmail.com>
2023-05-29 10:21:15 +0400

    landing page structure

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,9 +3,25 @@ import main from '../images/main.svg';
 
 const Landing = () => {
   return (
-    <div>
-      <img src={logo} alt="" />
-    </div>
+    <main>
+      <nav>
+        <img src={logo} className="logo" alt="jobster logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Stay organized and manage your job applications effectively. Add,
+            track, and delete job entries with ease. Never miss an opportunity
+            and stay on top of your job search.
+          </p>
+          <button className="btn btn-hero">login / register</button>
+        </div>
+        <img src={main} alt="main image" className="img main-img" />
+      </div>
+    </main>
   );
 };
 export default Landing;
