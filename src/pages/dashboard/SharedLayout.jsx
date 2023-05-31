7262675fa76bcc58dc86222dad476737abe7c692
Teona <tekizashvili@gmail.com>
2023-05-31 16:40:41 +0400

    complete sharedlayout

--- src/pages/dashboard/SharedLayout.jsx
+++ src/pages/dashboard/SharedLayout.jsx
@@ -1,4 +1,45 @@
+import styled from 'styled-components';
+import { SmallSidebar, BigSidebar, Navbar } from '../../components';
+import { Outlet } from 'react-router-dom';
+
 const SharedLayout = () => {
-  return <div>SharedLayout</div>;
+  return (
+    <Wrapper>
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  .dashboard {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+
+  .dashboard-page {
+    width: 90vw;
+    margin: 0 auto;
+    padding: 2rem 0;
+  }
+
+  @media (min-width: 992px) {
+    .dashboard {
+      grid-template-columns: auto 1fr;
+    }
+
+    .dashboard-page {
+      width: 90%;
+    }
+  }
+`;
+
 export default SharedLayout;
