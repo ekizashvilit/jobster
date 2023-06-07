1a8e4bcaeba0e2f5533a0471fddbb3cf15854a3a
Teona <tekizashvili@gmail.com>
2023-06-07 18:43:05 +0400

    setup alljobs page  structure

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,5 +4,16 @@ import BigSidebar from './BigSidebar';
 import Navbar from './Navbar';
 import SmallSidebar from './SmallSidebar';
 import FormRowSelect from './FormRowSelect';
+import SearchContainer from './SearchContainer';
+import JobsContainer from './JobsContainer';
 
-export { Logo, FormRow, BigSidebar, Navbar, SmallSidebar, FormRowSelect };
+export {
+  Logo,
+  FormRow,
+  BigSidebar,
+  Navbar,
+  SmallSidebar,
+  FormRowSelect,
+  JobsContainer,
+  SearchContainer,
+};
