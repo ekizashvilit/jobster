6b1c2589c12afbe810f05582e49059e6373ae017
Teona <tekizashvili@gmail.com>
2023-06-07 20:46:43 +0400

    container setup

--- src/components/JobsContainer.jsx
+++ src/components/JobsContainer.jsx
@@ -1,4 +1,65 @@
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import Job from './Job';
+import Loading from './Loading';
+
 const JobsContainer = () => {
-  return <div>JobsContainer</div>;
+  const { jobs, isLoading } = useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
+
+  if (isLoading) {
+    return (
+      <Wrapper>
+        <Loading />
+      </Wrapper>
+    );
+  }
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <h5>jobs info</h5>
+      <div className="jobs">
+        {jobs.map((item) => {
+          console.log(item);
+          return <Job key={item._id} {...item} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  margin-top: 4rem;
+
+  h2 {
+    text-transform: none;
+  }
+
+  & > h5 {
+    font-weight: 700;
+  }
+
+  .jobs {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 2rem;
+  }
+
+  @media (min-width: 992px) {
+    .jobs {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 1rem;
+    }
+  }
+`;
+
 export default JobsContainer;
