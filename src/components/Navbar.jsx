9bfe23df0bc0a775c17cfb25762c1026f7a1851b
Teona <tekizashvili@gmail.com>
2023-06-07 18:34:20 +0400

    alljobs slice setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
           <div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'}>
             <button
               className="dropdown-btn"
-              onClick={() => dispatch(logoutUser())}
+              onClick={() => dispatch(logoutUser('logged out'))}
             >
               logout
             </button>
