8f1e9f8d8369fc6a0fc28665346b6342f6ad4387
Teona <tekizashvili@gmail.com>
2023-06-13 12:20:37 +0400

    charts container

--- src/components/ChartsContainer.jsx
+++ src/components/ChartsContainer.jsx
@@ -1,4 +1,41 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
+
 const ChartsContainer = () => {
-  return <div>ChartsContainer</div>;
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useSelector((store) => store.allJobs);
+
+  return (
+    <Wrapper>
+      <h5>monthly applications</h5>
+      <button onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'area chart' : 'bar chart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  margin-top: 4rem;
+  text-align: center;
+
+  button {
+    background: transparent;
+    border-color: transparent;
+    text-transform: capitalize;
+    color: var(--primary-500);
+    font-size: 1.25rem;
+    cursor: pointer;
+  }
+
+  h4 {
+    text-align: center;
+    margin-bottom: 0.75rem;
+  }
+`;
+
 export default ChartsContainer;
