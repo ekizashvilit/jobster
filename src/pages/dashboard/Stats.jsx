dae65029852e283690862b70b2e6bc56a22436db
Teona <tekizashvili@gmail.com>
2023-06-13 11:56:16 +0400

    stats page structure

--- src/pages/dashboard/Stats.jsx
+++ src/pages/dashboard/Stats.jsx
@@ -1,15 +1,24 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { showStats } from '../../features/allJobs/allJobsSlice';
+import { StatsContainer, ChartsContainer } from '../../components';
 
 const Stats = () => {
   const dispatch = useDispatch();
+  const { isLoading, monthlyApplications } = useSelector(
+    (store) => store.allJobs
+  );
 
   useEffect(() => {
     dispatch(showStats());
   }, []);
 
-  return <div>Stats</div>;
+  return (
+    <>
+      <StatsContainer />
+      {monthlyApplications.length > 0 && <ChartsContainer />}
+    </>
+  );
 };
 
 export default Stats;
